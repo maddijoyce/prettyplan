54a01499cb28282a03bf0d0f6686c5a6413e9a96
Maddi Joyce <maddi@maddijoyce.com>
2019-08-08 13:01:17 +1000

    Use b64 encoded string in hash.

--- src/ts/prettyplan.ts
+++ src/ts/prettyplan.ts
@@ -1,29 +1,52 @@
-import { getCurrentVersion, getLastUsedVersion, updateLastUsedVersion } from './releases';
-import { expandAll, collapseAll, accordion, closeModal } from './ui';
-import { showReleaseNotification, hideReleaseNotification, showReleaseNotes, displayParsingErrorMessage, hideParsingErrorMessage, clearExistingOutput, unHidePlan, render } from './render';
-import { parse } from './parse';
+import {
+  getCurrentVersion,
+  getLastUsedVersion,
+  updateLastUsedVersion
+} from "./releases";
+import { expandAll, collapseAll, accordion, closeModal } from "./ui";
+import {
+  showReleaseNotification,
+  hideReleaseNotification,
+  showReleaseNotes,
+  displayParsingErrorMessage,
+  hideParsingErrorMessage,
+  clearExistingOutput,
+  unHidePlan,
+  render
+} from "./render";
+import { parse } from "./parse";
 
-window.addEventListener('load', function () {
-    if (getCurrentVersion() != getLastUsedVersion()) {
-        showReleaseNotification(getCurrentVersion());
-        updateLastUsedVersion();
-    }
-}); 
+window.addEventListener("load", function() {
+  if (getCurrentVersion() != getLastUsedVersion()) {
+    showReleaseNotification(getCurrentVersion());
+    updateLastUsedVersion();
+  }
+});
 
-(<any>window).runPrettyplan = () => {
-    hideParsingErrorMessage();
-    clearExistingOutput();
+const runPrettyplan = (terraformPlan: string) => {
+  hideParsingErrorMessage();
+  clearExistingOutput();
 
-    var terraformPlan = (<HTMLTextAreaElement>document.getElementById("terraform-plan")).value;
-    var plan = parse(terraformPlan);
+  var plan = parse(terraformPlan);
 
-    if (plan.warnings.length === 0 && plan.actions.length === 0) {
-        displayParsingErrorMessage();
-    }
+  if (plan.warnings.length === 0 && plan.actions.length === 0) {
+    displayParsingErrorMessage();
+  }
 
-    render(plan);
-    unHidePlan();
-}
+  render(plan);
+  unHidePlan();
+};
+(<any>window).runPrettyplan = runPrettyplan;
+
+window.onload = function() {
+  const b64_plan = window.location.hash.slice(1);
+
+  if (b64_plan) {
+    const plan = window.atob(b64_plan);
+    console.log(plan);
+    runPrettyplan(plan);
+  }
+};
 
 (<any>window).showReleaseNotes = showReleaseNotes;
 (<any>window).expandAll = expandAll;
